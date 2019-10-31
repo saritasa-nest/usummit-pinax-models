d6cd1d2926aa428c2cbd79832a7fc7c8bc187332
Alexander Dimov <alexander.dimov@saritasa.com>
2019-10-31 09:38:05 +0700

    Fix hard_delete in LogicalDelete
    Now hard_deelte get all objects for delete, even soft deleted

--- pinax/models/models.py
+++ pinax/models/models.py
@@ -67,16 +67,7 @@ class LogicalDeleteModel(models.Model):
         with using of ``LogicalDeleteCollector`` instead of ``Collector``.
 
         """
-        using = using or router.db_for_write(self.__class__, instance=self)
-        assert self._get_pk_val() is not None, (
-            "%s object can't be deleted because its %s attribute is set"
-            " to None." %
-            (self._meta.object_name, self._meta.pk.attname)
-        )
-
-        collector = LogicalDeleteCollector(using=using)
-        collector.collect([self], keep_parents=keep_parents)
-        return collector.delete()
+        super().delete()
 
     class Meta:
         abstract = True
--- pinax/models/query.py
+++ pinax/models/query.py
@@ -39,30 +39,4 @@ class LogicalDeleteQuerySet(QuerySet):
         with using of ``LogicalDeleteCollector`` instead of ``Collector``.
 
         """
-        assert self.query.can_filter(), \
-            "Cannot use 'limit' or 'offset' with delete."
-
-        if self._fields is not None:
-            raise TypeError(
-                "Cannot call delete() after .values() or .values_list()"
-            )
-
-        del_query = self._clone()
-
-        # The delete is actually 2 queries - one to find related objects,
-        # and one to delete. Make sure that the discovery of related
-        # objects is performed on the same database as the deletion.
-        del_query._for_write = True
-
-        # Disable non-supported fields.
-        del_query.query.select_for_update = False
-        del_query.query.select_related = False
-        del_query.query.clear_ordering(force_empty=True)
-
-        collector = LogicalDeleteCollector(using=del_query.db)
-        collector.collect(del_query)
-        deleted, _rows_count = collector.delete()
-
-        # Clear the result cache, in case this QuerySet gets reused.
-        self._result_cache = None
-        return deleted, _rows_count
+        return super().delete()
